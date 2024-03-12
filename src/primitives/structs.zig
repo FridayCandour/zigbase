25bb014ae210ad04dca39b7a772fd3b3bc1650b2
fridaycandour <fridaymichaels662@gmail.com>
2024-03-12 06:07:33 +0100

    Auto saved workflow

---
+++ src/primitives/structs.zig
@@ -0,0 +1,34 @@
+const ExabaseOptions = struct {
+    /// Exabase DBMS
+    /// ---
+    /// RCT Memory cache percentage
+    EXABASE_MEMORY_PERCENT: f32,
+
+    /// Exabase DBMS
+    /// ---
+    /// name
+    name: []const u8,
+
+    /// a url that points to another node this node can hydrate from if out of date
+    bearer: []const u8,
+
+    /// type of ring
+    mode: []const u8,
+
+    /// Exabase DBMS
+    /// ---
+    /// Data schemas
+    schemas: []Schema,
+
+    /// Exabase DBMS
+    /// ---
+    /// log each query?
+    logging: bool,
+
+    /// Exabase DBMS
+    /// ---
+    /// Exabase signing keys
+    EXABASE_KEYS: struct { privateKey: []const u8, publicKey: []const u8 },
+};
+
+const Schema = struct {};
